0fa59ae13e97e29de6e8bda67cc8e09b37d128c0
Nahuel Kerkaert <87397310+nahuelKer@users.noreply.github.com>
2021-09-01 19:17:04 -0300

    Update Atraccion.java

--- TP001 TierraMedia/src/tierramedia/Atraccion.java
+++ TP001 TierraMedia/src/tierramedia/Atraccion.java
@@ -5,12 +5,17 @@ public abstract class Atraccion {
 	private int costo;
 	private int tiempoMedio;
 	private int cupoMax;
+	
+	public Atraccion(){
+	}
+	
 
 	public Atraccion(String nombre, int costo, int tiempoMedio, int cupoMax) {
 		this.nombre = nombre;
 		this.costo = costo;
 		this.tiempoMedio = tiempoMedio;
 		this.cupoMax = cupoMax;
+		
 	}
 
 }
