b911e898e959e28ecec07a66d8986e4b28ffd6e1
diegoo-villalba <87017096+diegoo-villalba@users.noreply.github.com>
2021-09-01 19:55:55 -0300

    Update Promociones.java
    Agrego metodo "getCostoTotal" por si en algun momento puede ser de utilidad.

--- TP001 TierraMedia/src/tierramedia/Promociones.java
+++ TP001 TierraMedia/src/tierramedia/Promociones.java
@@ -1,5 +1,14 @@
 package tierramedia;
 
 public class Promociones {
+  
+  private Atraccion atraccion;
+  
+  Promociones(){ }
+  
+  public double getcostoTotal(Atraccion atraccion1, Atraccion atraccion2) {
+		
+		return atraccion1.getCosto() + atraccion2.getCosto();
+	}
 
 }
