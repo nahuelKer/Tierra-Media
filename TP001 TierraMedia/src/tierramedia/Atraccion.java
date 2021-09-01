9477b4ae498fa0e28e6e78712fc91e1a8cbfdd0a
Clara Casarré <87397457+casarreclara@users.noreply.github.com>
2021-09-01 19:35:26 -0300

    Update Atraccion.java

--- TP001 TierraMedia/src/tierramedia/Atraccion.java
+++ TP001 TierraMedia/src/tierramedia/Atraccion.java
@@ -5,20 +5,25 @@ public abstract class Atraccion {
 	private int costo;
 	private int tiempoMedio;
 	private int cupoMax;
+	private tipoDeAtraccion tipoAtraccion;
 
 	
 
-	public Atraccion(String nombre, int costo, int tiempoMedio, int cupoMax) {
+	public Atraccion(String nombre, int costo, int tiempoMedio, int cupoMax, tipoDeAtraccion tipoAtraccion) {
 		this.nombre = nombre;
 		this.costo = costo;
 		this.tiempoMedio = tiempoMedio;
 		this.cupoMax = cupoMax;
+		this.tipoAtraccion = tipoAtraccion;
 		
+	}
+	public TipoDeAtraccion getTipoAtraccion() {
+		return tipoAtraccion;
 	}
 	public static  List<Atraccion> listaDeAtracciones(Usuario usuario, List<Atraccion> atracciones) {
 		TipoDeAtraccion tipo = usuario.getAtraccion();
 		List <Atraccion> listaAtracciones = atracciones.stream()
-				.filter(atraccion -> atraccion.getTipo() == tipo)
+				.filter(atraccion -> atraccion.getTipoAtraccion() == tipo)
 				.collect(Collectors.toList());	
 		return listaAtracciones;
 	}
